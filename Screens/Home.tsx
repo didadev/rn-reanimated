23d517a09514dee5a512ee567fb9381464ee6f9b
Miladi Mohamed <miladi.dev@hotmail.com>
2024-02-05 21:57:39 +0100

    Add react navigation

---
+++ Screens/Home.tsx
@@ -0,0 +1,47 @@
+import {StyleSheet, Text, View, ScrollView, Pressable} from 'react-native';
+import {screens} from '../navigation/Navigator';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+const Home = ({navigation}) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <ScrollView
+      style={[
+        styles.container,
+        {
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+      ]}>
+      {screens.map(screen => (
+        <Pressable
+          key={screen.name}
+          style={styles.item}
+          onPress={() => navigation.navigate(screen.name)}>
+          <Text style={styles.title}>{screen.name}</Text>
+        </Pressable>
+      ))}
+    </ScrollView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {paddingHorizontal: 5, marginTop: 10},
+  item: {
+    marginVertical: 5,
+    padding: 15,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 10,
+  },
+  title: {
+    color: '#363636',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    fontSize: 18,
+  },
+});
---
+++ navigation/Navigator.tsx
@@ -0,0 +1,48 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import BottomSheetMansion from '../Youtube/ExampleAppMansion/index';
+import BottomSheet from '../Samples/BottomSheet';
+import InterpolateScrollView from '../Youtube/ Reactiive_channel/Interpolate_with_ScrollView/App';
+import InterpolateColors from '../Youtube/ Reactiive_channel/interpolateColors/App';
+import PinchGestureHandler from '../Youtube/ Reactiive_channel/PinchGestureHandler/App';
+import DoubleTapGesture from '../Youtube/ Reactiive_channel/doubleTapgesture/App';
+import CircularProgressBar from '../Youtube/ Reactiive_channel/circularProgressbar/App';
+import SwipeToDeleteAnimation from '../Youtube/ Reactiive_channel/SwipeToDeleteAnimation/App';
+import RippleEffect from '../Youtube/ Reactiive_channel/RippleEffect/App';
+import PerspectiveMenu from '../Youtube/ Reactiive_channel/perspectiveMenu/App';
+
+import Home from '../Screens/Home';
+
+export const screens = [
+  {name: 'bottom sheet Mansion', component: BottomSheetMansion},
+  {name: 'Simple bottom sheet', component: BottomSheet},
+  {name: 'Interpolate scrollview', component: InterpolateScrollView},
+  {name: 'Interpolate colors', component: InterpolateColors},
+  {name: 'Pinch gesture handler', component: PinchGestureHandler},
+  {name: 'double tap gesture', component: DoubleTapGesture},
+  {name: 'Circular Progressbar', component: CircularProgressBar},
+  {name: 'Swipe to delete', component: SwipeToDeleteAnimation},
+  {name: 'Ripple effect', component: RippleEffect},
+  {name: 'Perspective menu', component: PerspectiveMenu},
+];
+
+const Stack = createNativeStackNavigator();
+function Navigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerTintColor: 'white',
+        headerStyle: {backgroundColor: 'tomato'},
+        headerShown: false,
+        presentation: 'modal',
+      }}>
+      <Stack.Screen name="Home" component={Home} />
+      {screens.map(({name, component}) => (
+        <Stack.Screen key={name} name={name} component={component} />
+      ))}
+    </Stack.Navigator>
+  );
+}
+
+export default Navigator;
